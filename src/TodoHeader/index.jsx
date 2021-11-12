72c6ed8d6c15134d62eff66071343c918d04b2eb
Obed Paz <lesterpaz99@gmail.com>
2021-11-12 16:13:25 -0600

    create the Header component

---
+++ src/TodoHeader/index.jsx
@@ -0,0 +1,13 @@
+import { Children, cloneElement } from 'react';
+
+function TodoHeader({ children, loading }) {
+	return (
+		<header>
+			{Children.toArray(children).map((child) =>
+				cloneElement(child, { loading })
+			)}
+		</header>
+	);
+}
+
+export { TodoHeader };
